c39801636aa337ece05c2a07e21736b19f52ba65
harryhare <harryhare@163.com>
2020-12-03 12:50:55 +0800

    combo

--- 1_4_04.cpp
+++ 1_4_04.cpp
@@ -1,7 +1,7 @@
 /*
 ID: harryha7
 LANG: C++
-TASK: transform
+TASK: combo
 */
 #include <cstdio>
 #include <algorithm>
@@ -10,11 +10,50 @@ TASK: transform
 #include <cmath>
 
 using namespace std;
+
+bool check(int a, int b, int n) {
+    int x = (a - b + n) % n;
+    int y = (b - a + n) % n;
+    if (x >= 0 && x <= 2 || y >= 0 && y <= 2) {
+        return true;
+    }
+    return false;
+}
+
 int main() {
-    FILE *fin = fopen("transform.in", "r");
-    FILE *fout = fopen("transform.out", "w");
+    FILE *fin = fopen("combo.in", "r");
+    FILE *fout = fopen("combo.out", "w");
     int n;
     fscanf(fin, "%d", &n);
-    cout<<"template"<<endl;
+    if (n <= 5) {
+        fprintf(fout, "%d\n", n * n * n);
+        return 0;
+    }
+    int a[3];
+    int b[3];
+    for (int i = 0; i < 3; i++) {
+        fscanf(fin, "%d", a + i);
+    }
+    for (int i = 0; i < 3; i++) {
+        fscanf(fin, "%d", b + i);
+    }
+
+    int s = 1;
+    for (int i = 0; i < 3; i++) {
+        int x = min(a[i], b[i]);
+        int y = max(a[i], b[i]);
+        int t = 1;
+        if (y - x < 5) {
+            t = 5 - (y - x);
+        } else if (x + n - y < 5) {
+            t = 5 - (x + n - y);
+        } else {
+            s *= 0;
+        }
+        s *= t;
+    }
+    int r = 250 - s;
+    cout << r << endl;
+    fprintf(fout, "%d\n", r);
     return 0;
 }
